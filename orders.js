1fe4fd17b1a1833214921ca942187567b428038f
Kevin Salasblanca <61848667+kecin1299@users.noreply.github.com>
2022-07-11 19:52:37 -0600

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,47 @@
+const Orders = [
+    {
+
+        productName: 'Joya de diamante',
+        productNumber: '88745216',
+        paymentStatus: '--',
+        shipping: 'Pendiente',
+          
+    
+    
+    
+    },
+    {
+
+        productName: 'Joya de diamante',
+        productNumber: '88745216',
+        paymentStatus: '--',
+        shipping: 'Pendiente',
+          
+    
+    
+    
+    },
+    {
+
+        productName: 'Joya de diamante',
+        productNumber: '88745216',
+        paymentStatus: '--',
+        shipping: 'Pendiente',
+          
+    
+    
+    
+    },
+    {
+
+        productName: 'Joya de diamante',
+        productNumber: '88745216',
+        paymentStatus: '--',
+        shipping: 'Pendiente',
+          
+    
+    
+    
+    }
+    
+] 
